9b8cab436328603c1d43c88442ed2bcb7d460005
oluwabukola <daniellabaiyewu@gmail.com>
2020-03-26 19:17:40 +0100

    javascript_task

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = "Oluwabukola BAIYEWU";
+console.log(name);
+let courses = ['HTML', 'CSS', 'JAVASCRIPT'];
+console.log(courses);
+
+    for (let i = 0; i <= 200; i++){
+        if (courses.length %2 !==0 && i%2 !== 0) {
+            console.log(i);
+        }
+    }
